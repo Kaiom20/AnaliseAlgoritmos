7a12219f26221683af9967feacbbd56186893196
Kaio Márcio Araújo Cavalcante Lira <kaiomacl.20@gmail.com>
2025-05-26 12:22:05 -0300

    Atualiza Selection-sort

--- fontes/selection.c
+++ fontes/selection.c
@@ -1,39 +1,53 @@
+#include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-void selection_sort(int v[], int n) {
-    int i, j, m, temp;
-    for (i = 0; i < n - 1; i++) {
+void swap(int *a, int *b)
+{
+    int m;
+    m = *a;
+    *a = *b;
+    *b = m;
+}
+
+void selection_sort(int *v, unsigned int n)
+{
+    unsigned int i, j;
+    int m;
+
+    for (i = 0; i < (n - 1); i++)
+    {
         m = i;
-        for (j = i + 1; j < n; j++) {
-            if (v[m] > v[j]) {
+        for (j = (i + 1); j < n; j++)
+        {
+            if (v[m] > v[j])
                 m = j;
-            }
         }
-        if (m != i) {
-            temp = v[m];
-            v[m] = v[i];
-            v[i] = temp;
-        }
+        swap(&v[m], &v[i]);
     }
 }
 
-int main() {
-    int vetor[] = {9,8, 7, 6, 5, 4, 3, 2, 1};
-    int n = sizeof(vetor) / sizeof(vetor[0]);
+int main(int argc, char **argv)
+{
+    struct timespec a, b;
+    unsigned int t, n;
+    int i, *v;
 
-    printf("Vetor antes da ordenação: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", vetor[i]);
-    }
-    printf("\n");
+    n = atoi(argv[1]);
+    v = (int *) malloc(n * sizeof(int));
+    srand(time(NULL));
+    for (i = 0; i < n; i++)
+        v[i] = rand();
 
-    selection_sort(vetor, n);
+    clock_gettime(CLOCK_MONOTONIC, &b);
+    selection_sort(v, n);
+    clock_gettime(CLOCK_MONOTONIC, &a);
 
-    printf("Vetor depois da ordenação: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", vetor[i]);
-    }
-    printf("\n");
+    t = (a.tv_sec * 1e9 + a.tv_nsec) - (b.tv_sec * 1e9 + b.tv_nsec);
+
+    printf("%u\n", t);
+
+    free(v);
 
     return 0;
 }
